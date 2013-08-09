1c8407a4861a0fbfe5d71534e25f9c5af25057bd
Gordon Macie <gmacie@gmail.com>
2013-08-09 13:33:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Civicspaces::Application.config.secret_key_base = '74a2ab162bdb794158218fcab2efd6b4c1fb25d4f2f8ec61a2c09588e81f8361e60ebb2d2807009e2b7bbd9636a9df6681f85ff71d5387b8b18eb6de25612b52'
