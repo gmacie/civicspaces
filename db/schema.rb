e4dcf8411fa80095ec118fe3daccc1df75f30069
Gordon Macie <gmacie@gmail.com>
2013-12-04 13:11:33 -0500

    upgrade rails to 4.0.2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813175110) do
+ActiveRecord::Schema.define(version: 20131203184254) do
 
   create_table "cspaces", force: true do |t|
     t.string   "address1"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20130813175110) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
